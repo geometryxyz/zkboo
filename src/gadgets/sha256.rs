abf8753b8909eb12010ce6190ae4bd19bd30246a
Andrija <akinovak@gmail.com>
2023-03-08 21:10:49 +0100

    Implementation of full sha256
    Co-authored-by: ying tong <therealyingtong@users.noreply.github.com>

--- src/gadgets/sha256.rs
+++ src/gadgets/sha256.rs
@@ -4,8 +4,10 @@ mod iv;
 mod msg_schedule;
 mod test_vectors;
 
-use crate::gf2_word::GF2Word;
+use crate::{gf2_word::GF2Word, party::Party, error::Error};
 use std::ops::Deref;
+
+use self::{msg_schedule::{mpc_msg_schedule, mpc_msg_schedule_verify}, compression::{mpc_compression, mpc_compression_verify}, final_digest::{mpc_digest, mpc_digest_verify}};
 
 /// TODO: Doc
 #[derive(Debug)]
@@ -101,5 +103,166 @@ impl Deref for H {
     type Target = GF2Word<u32>;
     fn deref(&self) -> &Self::Target {
         &self.0
+    }
+}
+
+pub fn mpc_sha256(
+    input_p1: &[GF2Word<u32>; 16], 
+    input_p2: &[GF2Word<u32>; 16], 
+    input_p3: &[GF2Word<u32>; 16], 
+    p1: &mut Party<u32>,
+    p2: &mut Party<u32>,
+    p3: &mut Party<u32>,
+) -> (Vec<GF2Word<u32>>, Vec<GF2Word<u32>>, Vec<GF2Word<u32>>) {
+    let (msg_schedule_1, msg_schedule_2, msg_schedule_3) = mpc_msg_schedule(input_p1, input_p2, input_p3, p1, p2, p3);
+
+    let (compression_output_1, compression_output_2, compression_output_3) = mpc_compression(
+        &msg_schedule_1, &msg_schedule_2, &msg_schedule_3, p1, p2, p3
+    );
+
+    mpc_digest(&compression_output_1.try_into().unwrap(), &compression_output_2.try_into().unwrap(), &compression_output_3.try_into().unwrap(), p1, p2, p3)
+}
+
+pub fn mpc_sha256_verify(
+    input_p: &[GF2Word<u32>; 16], 
+    input_p_next: &[GF2Word<u32>; 16], 
+    p: &mut Party<u32>,
+    p_next: &mut Party<u32>,
+) -> Result<(Vec<GF2Word<u32>>, Vec<GF2Word<u32>>), Error> {
+    let (msg_schedule_p, msg_schedule_p_next) = mpc_msg_schedule_verify(input_p, input_p_next, p, p_next);
+
+    let (compression_output_p, compression_output_p_next) = mpc_compression_verify(
+        &msg_schedule_p, &msg_schedule_p_next, p, p_next
+    )?;
+
+    Ok(mpc_digest_verify(&compression_output_p.try_into().unwrap(), &compression_output_p_next.try_into().unwrap(), p, p_next))
+}
+
+#[cfg(test)]
+mod test_sha256 {
+    use std::{
+        fmt::{Debug, Display},
+        marker::PhantomData,
+        ops::{BitAnd, BitXor},
+    };
+
+    use rand::{rngs::ThreadRng, thread_rng};
+    use rand_chacha::ChaCha20Rng;
+    use sha3::Keccak256;
+    use sha2::{Digest, Sha256};
+
+    use crate::{
+        circuit::{Circuit, Output},
+        error::Error,
+        gf2_word::{BitUtils, BytesInfo, GF2Word, GenRand},
+        party::Party,
+        prover::Prover,
+        verifier::Verifier,
+    };
+
+    use super::*;
+
+    pub struct Sha256Circuit {
+        preimage: String
+    }
+
+    impl Circuit<u32> for Sha256Circuit {
+        fn compute(&self, input: &[GF2Word<u32>]) -> Vec<GF2Word<u32>> {
+            assert_eq!(input.len(), 0);
+            // create a Sha256 object
+            let mut hasher = Sha256::new();
+
+            // write input message
+            hasher.update(self.preimage.as_bytes());
+
+            // read hash digest and consume hasher
+            let digest = hasher.finalize().to_vec();
+
+            let mut res = Vec::with_capacity(8);
+            for i in 0..8 {
+                let word = u32::from_be_bytes(digest[(4 * i)..(4 * i + 4)].try_into().unwrap());
+                res.push(word.into())
+            }
+
+            res
+        }
+
+        fn compute_23_decomposition(
+            &self,
+            p1: &mut Party<u32>,
+            p2: &mut Party<u32>,
+            p3: &mut Party<u32>,
+        ) -> (Vec<GF2Word<u32>>, Vec<GF2Word<u32>>, Vec<GF2Word<u32>>) {
+            assert_eq!(p1.view.input.len(), 16);
+            assert_eq!(p2.view.input.len(), 16);
+            assert_eq!(p3.view.input.len(), 16);
+
+            let (o1, o2, o3) = mpc_sha256(
+                &p1.view.input.clone().try_into().unwrap(),
+                &p2.view.input.clone().try_into().unwrap(),
+                &p3.view.input.clone().try_into().unwrap(),
+                p1,
+                p2,
+                p3,
+            );
+            (o1.to_vec(), o2.to_vec(), o3.to_vec())
+        }
+
+        fn simulate_two_parties(
+            &self,
+            p: &mut Party<u32>,
+            p_next: &mut Party<u32>,
+        ) -> Result<(Output<u32>, Output<u32>), Error> {
+            assert_eq!(p.view.input.len(), 16);
+            assert_eq!(p_next.view.input.len(), 16);
+
+            let (o1, o2) = mpc_sha256_verify(
+                &p.view.input.clone().try_into().unwrap(),
+                &p_next.view.input.clone().try_into().unwrap(),
+                p,
+                p_next,
+            )?;
+
+            Ok((o1.to_vec(), o2.to_vec()))
+        }
+
+        fn party_output_len(&self) -> usize {
+            8
+        }
+
+        fn num_of_mul_gates(&self) -> usize {
+            let msg_schedule = 3 * 48;
+            let compression = 9 * 64;
+            let digest = 8;
+            
+            msg_schedule + compression + digest
+        }
+    }
+
+    #[test]
+    fn test_circuit() {
+        let mut rng = thread_rng();
+        const SIGMA: usize = 80;
+        let input: Vec<GF2Word<u32>> = crate::gadgets::sha256::test_vectors::TEST_INPUT
+            .iter()
+            .map(|&vi| vi.into())
+            .collect();
+
+        let circuit = Sha256Circuit {
+            preimage: String::from("abc")
+        };
+
+        let output = circuit.compute(&[]);
+        let expected_output = crate::gadgets::sha256::test_vectors::DIGEST_OUTPUT;
+        for (&word, &expected_word) in output.iter().zip(expected_output.iter()) {
+            assert_eq!(word.value, expected_word);
+        }
+
+        let proof = Prover::<u32, ChaCha20Rng, Keccak256>::prove::<ThreadRng, SIGMA>(
+            &mut rng, &input, &circuit, &output,
+        )
+        .unwrap();
+
+        Verifier::<u32, ChaCha20Rng, Keccak256>::verify(&proof, &circuit, &output).unwrap();
     }
 }
